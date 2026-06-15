086d60a9bf5b79b8e03d107a9081a71d4b02e6af
rendrap <rendrapradana@gmail.com>
2026-06-15 11:47:35 +0700

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module beginGo
+
+go 1.26.4
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// variables
+	var name string = "Rendra"
+	fmt.Printf("This is my name %s\n", name)
+
+	age := 41
+	fmt.Printf("This is my age %d\n", age)
+
+	var city string
+	city = "Bogor"
+	fmt.Printf("this is my city %s\n", city)
+
+}
